043c1aa272cf64d4ca28dc561aab499c5a1123ce
Max Sosnov <crazymax101@gmail.com>
2020-09-01 09:56:33 +0700

    Делает initial state

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,42 @@
 import Koa from "koa";
-import { userRoutes } from "./controllers/users/users";
-
+import Router from "koa-router";
+import axios from "axios";
 const app = new Koa();
+
+const router = new Router();
+
+router.get("/users/:id", async (ctx) => {
+  const id = ctx.params.id;
+
+  const { status, data } = await axios.get(
+    "http://jsonplaceholder.typicode.com/users/" + id
+  );
+
+  ctx.status = status;
+  ctx.body = convertUser(data);
+});
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  username: string;
+  phone: string;
+  website: string;
+}
+
+function convertUser(data: any): User {
+  return {
+    id: data.id,
+    name: data.name,
+    username: data.username,
+    email: data.email,
+    phone: data.phone,
+    website: data.website,
+  };
+}
+
+const userRoutes = router.routes();
 
 app.use(userRoutes);
 
